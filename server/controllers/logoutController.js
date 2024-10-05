c9a626743bc6363ac4b4971880e21c4613eed630
Sean <seanzhao2007@gmail.com>
2024-10-05 16:54:04 -0400

    changed cookie settings

--- server/controllers/logoutController.js
+++ server/controllers/logoutController.js
@@ -15,7 +15,7 @@ const handleLogout = async (req, res) => {
   const result = await foundUser.save();
   console.log(result);
 
-  res.clearCookie("jwt", { httpOnly: true, sameSite: "None", secure: true });
+  res.clearCookie("jwt", { httpOnly: true, sameSite: "None", secure: false });
   res.sendStatus(204);
 };
 
