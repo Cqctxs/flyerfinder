421f76cf1cc29ecf1a63fda3943ef8f18a6968a2
Sean <seanzhao2007@gmail.com>
2024-10-05 23:46:35 -0400

    fetch flyer by slug

--- client/app/flyer/[slug]/page.jsx
+++ client/app/flyer/[slug]/page.jsx
@@ -1,5 +1,21 @@
-export default function Page({params}){
-    return (
-      <div>My slug is: {params.slug}</div>
-    )
-  }
+import { useQuery } from "@tanstack/react-query";
+
+export default function Page({ params }) {
+  const fetchFlyer = async () => {
+    const res = await fetch(
+      `https://api.findflyerswith.us/flyer/${params.slug}`
+    );
+    return res.json();
+  };
+
+  const {
+    isLoading,
+    error,
+    data: response,
+  } = useQuery({
+    queryKey: ["flyerByID"],
+    queryFn: fetchFlyer,
+  });
+  const flyer = response?.flyer;
+  return <div>My slug is: {params.slug}</div>;
+}
